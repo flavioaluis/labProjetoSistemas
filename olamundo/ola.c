48d67422d4b7b4623dcce0382c680a1ef254dd9c
luisflavio <flavio.aluis@gmail.com>
2021-09-14 15:47:50 -0300

    feat: Checagem dos parâmetros passados para o programa
    O programa checa se os parâmetros foram corretamente passados para o programa.

---
+++ olamundo/ola.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+    char argumento[30];
+
+    strcpy(argumento, argc > 2 ? argv[1]: "Mundo");
+    printf("Ola, %s!", argumento);
+    return 0;
+}
